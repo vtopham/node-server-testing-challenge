7839b3b3522ad2b2a92bf7b38c8a51d87ade2dd6
Victoria  Topham <topham.victoria@gmail.com>
2020-05-21 09:12:14 -0700

    post test works

--- server.spec.js
+++ server.spec.js
@@ -1,6 +1,7 @@
-const request = require('supertest')
+const request = require('supertest');
 
-const server = require('./server.js')
+const server = require('./server.js');
+const router = require('./api/descriptions-router.js');
 
 describe('GET /', () => {
     it('has process.env DB_ENV as "testing"', () => {
@@ -10,5 +11,39 @@ describe('GET /', () => {
     it('has status code of 200', () => {
         return request(server).get('/')
             .expect(200)
-    })
+            .expect('content-type', /json/)
+            .then(res => {
+                expect(res.body.message).toBe("API is up and running")
+            });
+    });
 });
+
+server.use('/api/descriptions', router);
+
+//get descriptions tests
+describe('GET /', () => {
+
+    it('has status code of 200 and returns data', () => {
+        return request(server).get('/api/descriptions')
+            .expect(200)
+            .expect('content-type', /json/)
+            .then(res => {
+                expect(res.body.data[0].id).toBe(1)
+            })
+    });
+});
+
+//add descriptions tests
+describe('POST /', () => {
+
+    it('has status code of 201 and success message', () => {
+        return request(server).post('/api/descriptions')
+            .send({description: 'fun'})
+            .expect(201)
+            
+    });
+});
+
+//remove descriptions tests
+
+
